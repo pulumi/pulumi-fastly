189ba059c17f0149ae34d60773ead3949e9e2516
Alex Qiu <aqiu@pulumi.com>
2023-04-18 16:19:33 -0700

    Fix lint err

--- provider/resources.go
+++ provider/resources.go
@@ -132,7 +132,7 @@ func Provider() tfbridge.ProviderInfo {
 
 	err := x.ComputeDefaults(&prov, x.TokensSingleModule("fastly_", mainMod,
 		x.MakeStandardToken(mainPkg)).Unmappable("_acl_", "acl is capitalized to ACL"))
-	contract.AssertNoError(err)
+	contract.AssertNoErrorf(err, "auto token mapping failed")
 	err = x.AutoAliasing(&prov, prov.GetMetadata())
 	contract.AssertNoErrorf(err, "auto aliasing apply failed")
 
