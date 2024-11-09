126fc2335f2b4de33e362d6fff3872a7563df95a
guineveresaenger <guinevere@pulumi.com>
2024-11-08 16:20:33 -0800

    Deprecate WithPlanResourceChange

--- provider/resources.go
+++ provider/resources.go
@@ -72,10 +72,7 @@ func makeResource(mod string, res string) tokens.Type {
 // Provider returns additional overlaid schema and metadata associated with the provider..
 func Provider() tfbridge.ProviderInfo {
 	// Instantiate the Terraform provider
-	p := shimv2.NewProvider(fastly.Provider(),
-		shimv2.WithPlanResourceChange(func(tfResourceType string) bool {
-			return tfResourceType == "fastly_service_vcl"
-		}))
+	p := shimv2.NewProvider(fastly.Provider())
 
 	// Create a Pulumi provider mapping
 	prov := tfbridge.ProviderInfo{
